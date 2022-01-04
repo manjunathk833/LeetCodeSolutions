097bcace9267ac42d4fde313b4800c68b0f896e9
manjunath h k <manjunathhk833@gmail.com>
2022-01-04 09:16:33 +0530

    symmetric subtree python solution

---
+++ Top100Liked/SymmetricTree.py
@@ -0,0 +1,51 @@
+'''
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+
+
+Example 1:
+
+
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+Example 2:
+
+
+Input: root = [1,2,2,null,3,null,3]
+Output: false
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+
+
+Follow up: Could you solve it both recursively and iteratively?
+'''
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+  def isSymmetric(self, root):
+    if root is None:
+      return True
+    else:
+      return self.isMirror(root.left, root.right)
+
+  def isMirror(self, left, right):
+    if left is None and right is None:
+      return True
+    if left is None or right is None:
+      return False
+
+    if left.val == right.val:
+      outPair = self.isMirror(left.left, right.right)
+      inPiar = self.isMirror(left.right, right.left)
+      return outPair and inPiar
+    else:
+      return False
