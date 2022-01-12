899ef7010c713b574d47da6f7418fa1a216a699f
manjunath h k <manjunathhk833@gmail.com>
2022-01-12 20:49:27 +0530

    partition labels and permutations solutions

---
+++ Top100Liked/medium/ParatitionLabels.py
@@ -0,0 +1,56 @@
+'''
+763. Partition Labels
+Medium
+
+6097
+
+245
+
+Add to List
+
+Share
+You are given a string s. We want to partition the string into as many parts as possible so that each letter appears in at most one part.
+
+Note that the partition is done so that after concatenating all the parts in order, the resultant string should be s.
+
+Return a list of integers representing the size of these parts.
+
+
+
+Example 1:
+
+Input: s = "ababcbacadefegdehijhklij"
+Output: [9,7,8]
+Explanation:
+The partition is "ababcbaca", "defegde", "hijhklij".
+This is a partition so that each letter appears in at most one part.
+A partition like "ababcbacadefegde", "hijhklij" is incorrect, because it splits s into less parts.
+Example 2:
+
+Input: s = "eccbbbbdec"
+Output: [10]
+
+
+Constraints:
+
+1 <= s.length <= 500
+s consists of lowercase English letters.
+Accepted
+316,187
+Submissions
+402,712
+'''
+# Solution
+
+class Solution(object):
+    def partitionLabels(self, S):
+        last = {c: i for i, c in enumerate(S)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(S):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i - anchor + 1)
+                anchor = i + 1
+
+        return ans
