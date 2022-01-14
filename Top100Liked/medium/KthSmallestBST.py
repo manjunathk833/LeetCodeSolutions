910f6a2198f8fb7a03c110ff3b40472b05a71156
manjunath h k <manjunathhk833@gmail.com>
2022-01-14 19:03:54 +0530

    added Daily temperatures and kth smallest element in BST solutions

---
+++ Top100Liked/medium/KthSmallestBST.py
@@ -0,0 +1,51 @@
+'''
+230. Kth Smallest Element in a BST
+Medium
+
+5459
+
+107
+
+Add to List
+
+Share
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+
+
+Example 1:
+
+
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+Example 2:
+
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+'''
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        n = 0
+        stack = []
+        cur = root
+
+        while cur or stack:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop()
+            n += 1
+            if n == k:
+                return cur.val
+            cur = cur.right
+        return
