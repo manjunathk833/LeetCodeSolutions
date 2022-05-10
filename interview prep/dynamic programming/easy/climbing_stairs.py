6ea88e8c5d3927fec480e84f6d0fef8752170ed4
manjunath h k <manjunathhk833@gmail.com>
2022-05-10 10:34:58 +0530

    added explaination comments for the dp solutions

--- interview prep/dynamic programming/easy/climbing_stairs.py
+++ interview prep/dynamic programming/easy/climbing_stairs.py
@@ -1,10 +1,33 @@
-#need to restudy
+'''
+You are climbing a staircase. It takes n steps to reach the top.
 
-class Solution:
-    def climbStairs(self, n: int) -> int:
-        one, two = 1, 1
-        for i in range(n-1):
-            temp = one
-            one = one + two
-            two = temp
-        return one
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+'''
+
+'''
+Approach: 1)take last and last second stairs as 1, 1 each since there's one way = base case.
+            2)Consecutively add all the rest of the positions from the last to get the answer
+'''
+
+def climbStairs(n):
+    one, two = 1, 1
+    for i in range(n-1):
+        temp = one
+        one = one + two
+        two = temp
+    return one
+
+n = 5
+
+sol = climbStairs(n)
+print(sol)
--- interview prep/dynamic programming/easy/countingbits.py
+++ interview prep/dynamic programming/easy/countingbits.py
@@ -1,12 +1,32 @@
 #https://leetcode.com/problems/counting-bits/
 
-class Solution:
-    def countBits(self, n: int) -> List[int]:
-        dp = [0] * (n + 1)
-        offset = 1
+'''
+Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
-        for i in range(1, n + 1):
-            if offset * 2 == i:
-                offset = i
-            dp[i] = 1 + dp[i - offset]
-        return dp
+Example 1:
+
+Input: n = 2
+Output: [0,1,1]
+Explanation:
+0 --> 0
+1 --> 1
+2 --> 10
+'''
+'''
+for offset of every power of two add 1 + i - offset
+'''
+
+
+def countBits(n):
+    dp = [0] * (n + 1)
+    offset = 1
+
+    for i in range(1, n + 1):
+        if offset * 2 == i:
+            offset = i
+        dp[i] = 1 + dp[i - offset]
+    return dp
+
+n=2
+sol = countBits(n)
+print(sol)
