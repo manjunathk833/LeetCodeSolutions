acdd63fe76e9b1544c0c953aa3ed615ecf1e10c5
manjunath h k <manjunathhk833@gmail.com>
2022-05-26 22:31:50 +0530

    added sorted queue soln but need to modify

---
+++ interview prep/twopointers/sortedsquare.py
@@ -0,0 +1,67 @@
+#https://leetcode.com/problems/squares-of-a-sorted-array/submissions/
+
+def sortedSquares(self, A):
+    answer = [0] * len(A)
+    l, r = 0, len(A) - 1
+    while l <= r:
+        left, right = abs(A[l]), abs(A[r])
+        if left > right:
+            answer[r - l] = left * left
+            l += 1
+        else:
+            answer[r - l] = right * right
+            r -= 1
+    return answer
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+
+
+
+Example 1:
+
+Input: nums = [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Explanation: After squaring, the array becomes [16,1,0,9,100].
+After sorting, it becomes [0,1,9,16,100].
+Example 2:
+
+Input: nums = [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums is sorted in non-decreasing order.
+
+
+Follow up: Squaring each element and sorting the new array is very trivial, could you find an O(n) solution using a different approach?
+
+soln: https://leetcode.com/problems/squares-of-a-sorted-array/discuss/222079/Python-O(N)-10-lines-two-solutions-explained-beats-100
+'''
