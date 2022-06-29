efeaf0a06fbb43672bfb1e789aa932f27ce735e3
manjunath h k <manjunathhk833@gmail.com>
2022-06-29 18:54:04 +0530

    learnt longest substring problem

--- interview prep/sliding_window/longest_substring_non_repeating_characters.py
+++ interview prep/sliding_window/longest_substring_non_repeating_characters.py
@@ -1,14 +1,18 @@
 #https://www.youtube.com/watch?v=wiGpQwVHdE0 https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/
-class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        charSet = set()
-        l = 0
-        res = 0
 
-        for r in range(len(s)):
-            while s[r] in charSet:
-                charSet.remove(s[l])
-                l += 1
-            charSet.add(s[r])
-            res = max(res, r - l + 1)
-        return res
+def lengthOfLongestSubstring(s):
+    charSet = set()
+    l = 0
+    res = 0
+
+    for r in range(len(s)):
+        while s[r] in charSet:
+            charSet.remove(s[l])
+            l += 1
+        charSet.add(s[r])
+        res = max(res, r - l + 1)
+    return res
+
+str = 'abcabcbb'
+n = lengthOfLongestSubstring(str)
+print("Longest Substring %d for the string %s"%(n,str))
